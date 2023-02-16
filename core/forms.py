cf229a8a25d584b3bca4660c01bb9b19f4bd98d7
aelsir <ahmed.elsir.khalfalla@gmail.com>
2023-02-16 17:03:17 +0000

    finish the details page

--- core/forms.py
+++ core/forms.py
@@ -10,3 +10,8 @@ class CommentForm(forms.ModelForm):
             'email':'بريدك الالكتروني',
             'body': 'تعليقك',
         }
+        widgets = {
+            "name": forms.TextInput(attrs={"class": "form-control"}),
+            'email': forms.EmailInput(attrs={"class": "form-control"}),
+            'body': forms.Textarea(attrs={"class": "form-control"}),
+        }
